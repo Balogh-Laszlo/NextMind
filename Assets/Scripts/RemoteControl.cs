b0c55e0447416f7da3282cf468e26d5299b45d91
Balogh-Laszlo <79311989+Balogh-Laszlo@users.noreply.github.com>
2022-03-08 17:07:03 +0200

    feat: remote control design

--- Assets/Scripts/RemoteControl.cs
+++ Assets/Scripts/RemoteControl.cs
@@ -2,11 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class RemoteControl : MonoBehaviour
 {
     public TMP_Text name;
     public Controller controller;
+
+    public GameObject button1;
+    public GameObject button2;
+    public GameObject button3;
+    public GameObject button4;
+    public GameObject button5;
+
+    private List<GameObject> buttons;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,6 +24,36 @@ public class RemoteControl : MonoBehaviour
         {
             controller = SelectController.currentController;
             name.text = controller.controllerName;
+            buttons = new List<GameObject>();
+            buttons.Add(button1);
+            buttons.Add(button2);
+            buttons.Add(button3);
+            buttons.Add(button4);
+            buttons.Add(button5);
+            for (int i = 0; i < 5; i++)
+            {
+                if (i < controller.controls.Count)
+                {
+                    TMP_Text text = buttons[i].GetComponentInChildren<TMP_Text>();
+                    if (text != null)
+                    {
+                        if (controller.controls[i] != null && controller.controls[i].ControlName != null)
+                        {
+                            text.text = controller.controls[i].ControlName;
+                        }
+                        else
+                        {
+                            buttons[i].SetActive(false);
+                        }
+                        
+                    }
+                }
+                else
+                {
+                    
+                    buttons[i].SetActive(false);
+                }
+            }
         }
     }
 
@@ -21,5 +61,10 @@ public class RemoteControl : MonoBehaviour
     void Update()
     {
         
+    }
+
+    public void onBackPressed()
+    {
+        SceneManager.LoadScene(12);
     }
 }
