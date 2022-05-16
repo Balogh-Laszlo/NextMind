0d59e2870615d31028b23142e05849d42e716818
Balogh-Laszlo <79311989+Balogh-Laszlo@users.noreply.github.com>
2022-05-16 15:18:12 +0300

    feat: registration UI

--- Assets/Scripts/Auth.cs
+++ Assets/Scripts/Auth.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using DefaultNamespace;
 using API;
 using DefaultNamespace.Models;
+using Models;
 using TMPro;
 using UnityEditor.PackageManager;
 using UnityEngine;
@@ -13,13 +14,25 @@ public class Auth : MonoBehaviour
 {
     // Start is called before the first frame update
     private IAPIHelper api;
+    
+    //Login
     public TMP_InputField userNameInput;
     public TMP_InputField passwordInput;
     public GameObject errorMessage;
+    private TMP_Text errorMessageText;
+    public GameObject loginObject;
+    
+    //Register
+    public GameObject registerObject;
+    public TMP_InputField userNameInputReg;
+    public TMP_InputField passwordInputReg;
+    public TMP_InputField confirmPasswordInputReg;
+    public TMP_Text regSuccessfulText;
     void Start()
     {
         // Dependency injection
         api = APIHelper.Instance;
+        errorMessageText = errorMessage.GetComponent<TMP_Text>();
         // RegisterRequest request = new RegisterRequest();
         // request.UserName = "test3";
         // request.Password = "test";
@@ -46,37 +59,145 @@ public class Auth : MonoBehaviour
         
     }
 
-    public void onLoginPressed()
+    public void OnLoginPressed()
     {
         Debug.Log("Login pressed");
         string username = userNameInput.text;
         string password = passwordInput.text;
-        if (username != null && password != null)
-        {
-            if (username.Length > 0 && password.Length > 0)
-            {
-                var loginRequest = new LoginRequest();
+        if ((username != null && password != null) && (username.Length >= 1 && password.Length >= 1)){
+            
+            Debug.Log(username);
+            var loginRequest = new LoginRequest();
                 loginRequest.UserName = username;
                 loginRequest.Password = password;
 
                 StartCoroutine(api.Login(loginRequest, (response) =>
                 {
-                    if (response.Code == 200)
+                    if (response != null)
                     {
-                        errorMessage.SetActive(false);
-                        api.Token = response.Token;
-                        api.UserName = username;
-                        SceneManager.LoadScene(1);
+                        if (response.Code == 200)
+                        {
+                            errorMessage.SetActive(false);
+                            api.Token = response.Token;
+                            api.UserName = username;
+                            SceneManager.LoadScene(1);
+                        }
+                        else
+                        {
+                            errorMessage.SetActive(true);
+
+                            errorMessageText.text = response.Message;
+                        }
                     }
                     else
                     {
                         errorMessage.SetActive(true);
-                        var errorMessageText = errorMessage.GetComponent<TMP_Text>();
-                        errorMessageText.text = response.Message;
+                        errorMessageText.text = "Something went wrong. Try again later!";
                     }
                 }));
-            }
+        }
+        else
+        {
+            errorMessage.SetActive(true);
+            errorMessageText.text = "Missing username or password!";
         }
         
+    }
+
+    public void OnSignUpPressed()
+    {
+        errorMessage.SetActive(false);
+        loginObject.SetActive(false);
+        registerObject.SetActive(true);
+        
+    }
+
+    public void OnBackPressed()
+    {
+        errorMessage.SetActive(false);
+        loginObject.SetActive(true);
+        registerObject.SetActive(false);
+    }
+
+    public void OnRegisterPressed()
+    {
+        string username = userNameInputReg.text;
+        string password = passwordInputReg.text;
+        string confirmPassword = confirmPasswordInputReg.text;
+
+        if (string.IsNullOrWhiteSpace(username))
+        {
+            errorMessageText.text = "Missing username";
+            errorMessage.SetActive(true);
+        }
+
+        else if(string.IsNullOrEmpty(password))
+        {
+            errorMessage.SetActive(true);
+            errorMessageText.text = "Missing password";
+        }
+        else if (password.Length < 5)
+        {
+            errorMessage.SetActive(true);
+            errorMessageText.text = "Password needs to be at least 6 characters long!";
+        }
+
+        else if (string.IsNullOrEmpty(confirmPassword))
+        {
+            errorMessage.SetActive(true);
+            errorMessageText.text = "Missing confirmation password";
+        }
+
+        else if (confirmPassword != password)
+        {
+            errorMessage.SetActive(true);
+            errorMessageText.text = "The two passwords are not the same!";
+        }
+        else
+        {
+            var request = new RegisterRequest();
+            request.UserName = username;
+            request.Password = password;
+            request.ConfirmPassword = confirmPassword;
+
+            StartCoroutine(api.Register(request, (response) =>
+            {
+                if (response != null)
+                {
+                    if (response.Code == 200)
+                    {
+                        errorMessage.SetActive(false);
+                        registerObject.SetActive(false);
+                        loginObject.SetActive(true);
+                        regSuccessfulText.alpha = 1;
+                        StartCoroutine(FadeTextToZeroAlpha(3f, regSuccessfulText));
+
+                    }
+                    else
+                    {
+                        errorMessage.SetActive(true);
+                        errorMessageText.text = response.Message;
+                    }
+                }
+                else
+                {
+                    errorMessage.SetActive(true);
+                    errorMessageText.text = "Something went wrong. Try again later!";
+                }
+            }));
+        }
+
+
+    }
+    
+    //from https://forum.unity.com/threads/fading-in-out-gui-text-with-c-solved.380822/
+    private IEnumerator FadeTextToZeroAlpha(float t, TMP_Text i)
+    {
+        i.color = new Color(i.color.r, i.color.g, i.color.b, 1);
+        while (i.color.a > 0.0f)
+        {
+            i.color = new Color(i.color.r, i.color.g, i.color.b, i.color.a - (Time.deltaTime / t));
+            yield return null;
+        }
     }
 }
